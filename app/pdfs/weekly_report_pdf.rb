217429acc222e585abacdfe482529162af3ae8b6
AimableHonorable <aimatwiri@gmail.com>
2021-04-09 23:46:19 +0200

    styling report tables and nested attributes

--- app/pdfs/weekly_report_pdf.rb
+++ app/pdfs/weekly_report_pdf.rb
@@ -4,7 +4,6 @@ class WeeklyReportPdf < Prawn::Document
     super(top_margin: 90)
     @weekly_reports = WeeklyReport.all
     header
-
     line_items
   end
   def header
@@ -14,16 +13,26 @@ class WeeklyReportPdf < Prawn::Document
     text "Tin: 106780328 \n Tel: (+250) 788827582 \n E-mail:tphilos2020@gmail.com", :size => 8
     text_box "From...... To......\n Names: Twiringiyimana Aimable ", :at => [380, 680], :size => 8
   end
-  def title
-
-  end
   def line_items
-    table line_item_rows
+    move_down(10)
+    table line_item_rows, :header => true,
+    :cell_style => { size: 8 },
+    :column_widths => {2 => 100, 3 => 100}
   end
   def line_item_rows
-    [["Date", "what you learnt", "Challenge"]] +
+    [["Date", "site", "what you learnt", "Challenge"]] +
     @weekly_reports.map do |item|
-      [item.done_at, item.lesson, item.challenge]
+      [item.done_at, activity_items(item), item.lesson, item.challenge]
+    end
+  end
+  def activity_items(item)
+    make_table activity_item_rows(item),
+    :cell_style => {size: 8, :borders => [:left, :bottom]},
+    :column_widths => {0 => 40, 1 => 60, 2 => 150}
+  end
+  def activity_item_rows(item)
+    item.activities.map do |activity|
+      [activity.site," #{activity.to.to_s(:time)} - #{activity.to.to_s(:time)}",activity.action]
     end
   end
 end
